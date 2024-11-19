f80c3c69c56cba8679d7e021e9c0a4b229cf8826
Jari Honkanen <jaarhonka@yahoo.com>
2024-11-19 07:32:47 +0200

    add testbench for 3ph lcr simulation

--- vunit_run_ode.py
+++ vunit_run_ode.py
@@ -7,11 +7,11 @@ from vunit import VUnit
 ROOT = Path(__file__).resolve().parent
 VU = VUnit.from_argv()
 
-
-#this is obsolete and will be rewritten
 ode = VU.add_library("ode")
 ode.add_source_files(ROOT / "write_pkg.vhd")
 ode.add_source_files(ROOT / "ode_solvers/ode_pkg.vhd")
+
 ode.add_source_files(ROOT / "testbenches/lcr_simulation_rk4_tb.vhd")
+ode.add_source_files(ROOT / "testbenches/lcr_3ph_tb.vhd")
 
 VU.main()
