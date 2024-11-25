49f1087638ab04bdeffccb686148e6c430647e1f
Jari Honkanen <jaarhonka@yahoo.com>
2024-11-25 06:20:33 +0200

    start adding dop54 routine

--- ode_solvers/ode_pkg.vhd
+++ ode_solvers/ode_pkg.vhd
@@ -32,6 +32,16 @@ package ode_pkg is
         simtime  : inout real;
         err      : inout real;
         stepsize : inout real);
+
+------------------------------------------
+    procedure generic_adaptive_dop54
+    generic(impure function deriv (input : real_vector) return real_vector is <>)
+    (
+        state    : inout real_vector;
+        z_n1     : inout real_vector;
+        simtime  : inout real;
+        err      : inout real;
+        stepsize : inout real);
 ------------------------------------------
     procedure generic_rk4
     generic(impure function deriv (input : real_vector) return real_vector is <>)
@@ -148,9 +158,10 @@ package body ode_pkg is
         variable k : state_array;
         variable y_n1 : real_vector(state'range);
 
-        variable tolerance : real := 1.0e-4;
+        variable tolerance : real := 1.0e-3;
         variable h         : real := stepsize;
         variable h_new     : real ;
+        variable vErr       : real_vector(state'range);
 
     begin
         k(1) := z_n1;
@@ -161,15 +172,15 @@ package body ode_pkg is
 
         k(4) := deriv(y_n1);
 
-        z_n1 := state + (k(1)*7.0/24.0 + k(2)*1.0/4.0 + k(3)*1.0/3.0 + k(4) * 1.0/8.0) * stepsize;
-
-        err := abs((z_n1(0) - y_n1(0)));
-
+        z_n1  := state + k(4) * stepsize;
         state := y_n1;
 
-        if err < tolerance then
-            simtime := simtime + stepsize;
-        end if;
+        vErr := (k(1)*(-5.0/72.0) + k(2)*1.0/12.0 + k(3)*1.0/9.0 - k(4)*1.0/8.0) * stepsize;
+
+        err := abs(vErr(0)); -- use max value
+
+        -- if err < tolerance then
+        -- end if;
 
         -- cubic root
         h_new := h*cbrt(tolerance/err);
@@ -179,9 +190,101 @@ package body ode_pkg is
         if h_new > 100.0e-6 then
             h_new := 100.0e-6;
         end if;
+
+        simtime  := simtime + stepsize;
         stepsize := h_new;
 
     end generic_adaptive_rk23;
+
+------------------------------------------
+    procedure generic_adaptive_dop54
+    generic(impure function deriv (input : real_vector) return real_vector is <>)
+    (
+        state    : inout real_vector;
+        z_n1     : inout real_vector;
+        simtime  : inout real;
+        err      : inout real;
+        stepsize : inout real
+    ) is
+        type state_array is array(1 to 7) of real_vector(state'range);
+        variable k : state_array;
+        variable y_n1 : real_vector(state'range);
+
+        variable tolerance : real := 1.0e-3;
+        variable h         : real := stepsize;
+        variable h_new     : real ;
+        variable vErr       : real_vector(state'range);
+
+        constant dop2 : real_vector := (0 => 1.0/5.0);
+        constant dop3 : real_vector := (3.0/40.0       , 9.0/40.0);
+        constant dop4 : real_vector := (3.0/40.0       , 9.0/40.0        , 32.0/9.0);
+        constant dop5 : real_vector := (19372.0/6561.0 , -25360.0/2187.0 , 64448.0/6561.0 , -212.0/729.0);
+        constant dop6 : real_vector := (9017.0/3168.0  , -355.0/33.0     , 46732.0/5247.0 , 49.0/176.0     , -5103.0/18656.0);
+        constant dop7 : real_vector := (35.0/384.0     , 0.0             , 500.0/1113.0   , 125.0/192.0    , -2187.0/6784.0    , 11.0/84.0);
+
+    begin
+        k(1) := z_n1;
+        k(2) := deriv(state +
+            ( k(1) * dop2(0) 
+            ) * stepsize);
+        k(3) := deriv(state +
+            ( k(1) * dop3(0)
+            + k(2) * dop3(1)
+            ) * stepsize);
+        k(4) := deriv(state +
+            ( k(1) * dop4(0)
+            + k(2) * dop4(1)
+            + k(3) * dop4(2)
+            ) * stepsize);
+
+        k(5) := deriv(state +
+            ( k(1) * dop5(0)
+            + k(2) * dop5(1)
+            + k(3) * dop5(2)
+            + k(4) * dop5(3)
+            ) * stepsize);
+
+        k(6) := deriv(state +
+            ( k(1) * dop6(0)
+            + k(2) * dop6(1)
+            + k(3) * dop6(2)
+            + k(4) * dop6(3)
+            + k(5) * dop6(4)
+            ) * stepsize);
+
+        k(7) := deriv(state +
+            ( k(1) * dop7(0)
+            + k(2) * dop7(1)
+            + k(3) * dop7(2)
+            + k(4) * dop7(3)
+            + k(5) * dop7(4)
+            + k(6) * dop7(5)
+            ) * stepsize);
+
+
+        -- y_n1  := state + 
+        state := state + k(7) * stepsize;
+
+        -- vErr := k(7) - 
+
+        err := abs(vErr(0)); -- use max value
+
+        -- if err < tolerance then
+        -- end if;
+
+        -- cubic root
+        h_new := h*cbrt(tolerance/err);
+        if h_new < 100.0e-9 then
+            h_new := 100.0e-9;
+        end if;
+        if h_new > 100.0e-6 then
+            h_new := 100.0e-6;
+        end if;
+
+        simtime  := simtime + stepsize;
+        stepsize := h_new;
+
+    end generic_adaptive_dop54;
 
 ------------------------------------------
     procedure generic_rk4
