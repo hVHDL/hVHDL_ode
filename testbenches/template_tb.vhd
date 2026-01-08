75dfa78fd44190d07ffa402597591d377764e25e
Jari Honkanen <jaarhonka@yahoo.com>
2026-01-08 17:28:56 +0200

    clean up simulation code

--- testbenches/template_tb.vhd
+++ testbenches/template_tb.vhd
@@ -52,7 +52,7 @@ begin
 
         variable sw_frequency : real := 200.0e3;
         variable t_sw : real := 1.0/sw_frequency;
-        variable duty : real := 0.5;
+        variable duty : real := 0.6;
 
         ----------------------
         function fc_modulator
@@ -82,13 +82,6 @@ begin
         constant n_half : bit_vector(1 downto 0) := "01";
         constant zero   : bit_vector(1 downto 0) := "00";
 
-        type sw_state_record is record
-            sw_state : sw_states;
-            next_sw_state : sw_states;
-        end record;
-
-        constant init_sw_state : sw_state_record := (dc, p_half);
-
         variable sw_state      : sw_states := dc;
         variable next_sw_state : sw_states := p_half;
         variable prev_sw_state : sw_states := n_half;
@@ -115,12 +108,11 @@ begin
             return next_sw_state;
         end get_next_sw_state;
 
-        variable fc_duty : real := 1.0;
-
         ----------
         impure function get_step_length return real is
             variable step_length : real := 1.0e-9;
         begin
+
             case sw_state is
                 WHEN dc     => step_length := t_sw * duty;
                 WHEN p_half => step_length := t_sw * (1.0-duty);
@@ -132,35 +124,36 @@ begin
 
         end get_step_length;
         ----------
-        impure function get_bridge_voltage(sw_state : sw_states ; udc : real; ufc : real_vector) return real is
+        function get_bridge_voltage(sw_state : sw_states ; udc : real; ufc : real_vector) return real is
             variable bridge_voltage : real := 0.0;
         begin
-            CASE sw_state is
-                WHEN dc     => bridge_voltage := udc;
-                WHEN p_half => bridge_voltage := udc-ufc(0);
-                WHEN n_half => bridge_voltage := ufc(0);
-                WHEN zero   => bridge_voltage := 0.0;
-            end CASE;
+
+            bridge_voltage := 
+                fc_modulator(('0', sw_state(1)))   * udc
+              + fc_modulator(sw_state(1 downto 0)) * ufc(0);
 
             return bridge_voltage;
         end get_bridge_voltage;
 
         ----------
         impure function deriv_lcr(t : real; states : real_vector) return real_vector is
-            variable retval : init_state_vector'subtype := init_state_vector;
+            variable retval : states'subtype := (others => 0.0);
             variable bridge_voltage : real := 0.0;
+            alias il is states(0);
+            alias uc is states(1);
+            alias ufc1 is states(2);
         begin
 
             if t > 250.0e-6 then i_load := 10.0; end if;
-            if t > 600.0e-6 then duty := 0.8; end if;
+            if t > 600.0e-6 then duty := 0.4; end if;
 
             bridge_voltage := 
-                fc_modulator(('0', sw_state(1))) * udc
-              + fc_modulator((sw_state(1), sw_state(0))) * states(2);
+                fc_modulator(('0', sw_state(1)))   * udc
+              + fc_modulator(sw_state(1 downto 0)) * ufc1;
 
-            retval(0) := (bridge_voltage - states(0) * rl - states(1)) * (1.0/l);
-            retval(1) := (states(0)      - i_load) * (1.0/c);
-            retval(2) := fc_modulator((sw_state(1), sw_state(0))) * states(0);
+            retval(0) := (bridge_voltage - il * rl - uc) * (1.0/l);
+            retval(1) := (il - i_load) * (1.0/c);
+            retval(2) := -fc_modulator(sw_state(1 downto 0)) * il / cfc;
 
             return retval;
 
